36cc80e6565b6ae8646c6f11022c9cb8876a076b
Sumito Izumita <sumito@izumita.com>
2020-09-12 21:53:15 +0900

    10分に一度動くバッチ処理の作成

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 from discord.ext import commands
 from os import environ
 from lib import db
+from models import CryptoModel, Crypto
 from sqlalchemy.engine.url import URL
+import asyncio
 
 
 class Cryptrans(commands.Bot):
     def __init__(self) -> None:
         super().__init__(command_prefix=commands.when_mentioned_or("vc."))
         self.loop.create_task(self.init_db())
+        self.loop.create_task(self.give_hold_batch())
 
     async def init_db(self):
         await db.set_bind(
@@ -21,3 +24,13 @@ class Cryptrans(commands.Bot):
             )
         )
         await db.gino.create_all()
+
+    async def give_hold_batch(self):
+        await self.wait_until_ready()
+        while not self.is_closed():
+            await asyncio.sleep(60 * 10)
+
+            await asyncio.gather(
+                *[crypto.update(hold=Crypto.hold + crypto.per_amount).apply() for crypto in await CryptoModel().all()],
+                loop=self.loop
+            )
