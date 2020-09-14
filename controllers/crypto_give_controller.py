b031476529602668125dcb6e16d01929569eaad0
Sumito Izumita <sumito@izumita.com>
2020-09-14 18:52:04 +0900

    aliasの追加

--- controllers/crypto_give_controller.py
+++ controllers/crypto_give_controller.py
@@ -43,7 +43,7 @@ class CryptoGiveController(commands.Cog):
 
         await EmbedMaker(ctx).by_error_text("処理に失敗しました。再度実行してください。").send()
 
-    @commands.command(aliases=["pay"])
+    @commands.command(aliases=["pay", "send"])
     async def tip(self, ctx: commands.Context, member: discord.Member, unit: str, amount: int):
         """
         unitで指定した通貨を渡します。
